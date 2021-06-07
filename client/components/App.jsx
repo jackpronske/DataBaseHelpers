fb8f3e1fbd0d73b3be69130f45e4b5516e1fcf76
Jack <jackpronske@yahoo.com>
2021-06-07 12:05:23 -0700

    --implemented get and put request connections between front end and back end --updating still needs to be adjusted on the front end, but the connection is there

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -12,6 +12,21 @@ class App extends React.Component {
     };
     this.postNewContact = this.postNewContact.bind(this);
     this.deleteContact = this.deleteContact.bind(this);
+    this.getAllContacts = this.getAllContacts.bind(this);
+  }
+
+  componentDidMount() {
+    this.getAllContacts();
+  }
+
+  getAllContacts() {
+    axios.get('/contacts')
+      .then((results) => {
+        console.log('here are the get results', results)
+      })
+      .catch((error) => {
+        console.error('oops there was an error', error)
+      })
   }
 
   postNewContact(formObject) {
@@ -29,7 +44,18 @@ class App extends React.Component {
     //delete request to server with id of the contact we're deleting
     axios.delete(`/contacts/${id}`)
       .then((results) => {
+        //can either call a get request to retrieve refreshed data here, or handle that on the server side on the success of the deletion of the current id
         console.log('deleted', results);
+      })
+      .catch((error) => {
+        console.error('there was an error', error);
+      })
+  }
+
+  updateContact(id, changesObj) {
+    axios.put(`/contacts/${id}`, changesObj)
+      .then((results) => {
+        console.log('updated', results);
       })
       .catch((error) => {
         console.error('there was an error', error);
